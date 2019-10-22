fef9d78b7781223972b1f1fffb9176b2cc994105
Chris Dieter <c.dieter@lillylcom>
2019-10-22 15:10:37 -0400

    ui update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/header.js"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header headerText="Welcome" />
+    <Header headerText="New Features" />
     <p>Such wow. Very React.</p>
   </div>
 )
