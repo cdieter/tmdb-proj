f9280e85c0230aebcd8244a066a24cee843c4ad6
Chris Dieter <c.dieter@lillylcom>
2019-11-12 14:30:15 -0500

    adding typography;

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,1 +1,4 @@
-module.exports = []
+module.exports = [{
+      plugin: require('/Users/c232572/workspace/tmdb-proj/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
+    }]
