24eef1e096926f02c8bbeca72c5ba7ed7c89d7b7
Chris Dieter <c.dieter@lillylcom>
2019-11-13 13:22:58 -0500

    forms

--- src/components/inputForm.js
+++ src/components/inputForm.js
@@ -1,34 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default class InputForm extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {value: ``, finalValue: ``};
-
+    this.state = {
+                  value: ``, 
+                  finalValue: ``,
+                  job_value: ``,
+                }            
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleChange(section, event) {
+    this.setState({value: event.target.value}); 
+    console.log(section);
   }
 
   handleSubmit(event) {
     this.setState({finalValue: this.state.value});
     event.preventDefault();
   }
-
+  
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label style={{padding: `0 1rem` }}>
           <h3>Name:</h3>
-          <input type="text" value={this.state.value} onChange={this.handleChange}/>
-        </label>
-        <label style={{padding: `0 1rem` }}>
-          <h3>Job:</h3>
-          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          <input type="text" onChange={(e) => this.handleChange("tag", e)}/>
         </label>
         <input type="submit" value="Submit" />
         <p>Name: {this.state.finalValue}</p>
